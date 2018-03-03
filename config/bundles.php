3f6fbe035c5138cb48bcaf337206a34d7cd45263
Kevin Porras <kporras07@gmail.com>
2018-03-03 14:33:13 -0600

    Install JMSSerializerBundle.

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
