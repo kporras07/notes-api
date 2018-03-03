1172577d24428bd54957f6017fe03aabcb3900cf
Kevin Porras <kporras07@gmail.com>
2018-03-03 14:06:40 -0600

    Finish FOSUserBundle config.

---
+++ src/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * Get the value of Id
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
