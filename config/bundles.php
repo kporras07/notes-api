eaf5279fd9e50b3cd6de8fff45aa238947dd5da4
Kevin Porras <kporras07@gmail.com>
2018-03-03 14:40:46 -0600

    Install and configure nelmio api doc.

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
