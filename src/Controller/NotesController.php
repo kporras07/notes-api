28500c1d7cb07b4ade5d564ea88ce9131b2763ea
Kevin Porras <kporras07@gmail.com>
2018-03-21 22:24:06 -0600

    Add PUT action.

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -168,4 +168,82 @@ class NotesController extends FOSRestController
       $view = $this->view($note, 201);
       return $this->handleView($view);
     }
+
+    /**
+     * Notes PUT.
+     *
+     * @SWG\Put(
+     *     security={{"bearer":{}}},
+     *     tags={"Notes"},
+     *     @SWG\Response(
+     *         response=201,
+     *         description="Successfully created",
+     *         @SWG\Schema(
+     *             type="array",
+     *             @Model(type=Note::class)
+     *         )
+     *     ),
+     *     @SWG\Response(
+     *         response=204,
+     *         description="Successfully updated"
+     *     ),
+     *     @SWG\Parameter(
+     *         name="note",
+     *         in="body",
+     *         description="The note to create",
+     *         type="Note::class",
+     *         @SWG\Schema(
+     *             @SWG\Property(
+     *                 property="id",
+     *                 description="Note's id",
+     *                 type="integer",
+     *             ),
+     *             @SWG\Property(
+     *                 property="title",
+     *                 description="Note's title",
+     *                 type="string",
+     *             ),
+     *             @SWG\Property(
+     *                 property="body",
+     *                 description="Note's body",
+     *                 type="string",
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function putNotesAction(Request $request)
+    {
+      $note_id = $request->get('id');
+      $response_code = 204;
+      if ($note_id) {
+        $user = $this->getUser();
+        $note = $this->noteRepository->find($note_id);
+        if (!empty($note)) {
+          if (!$user->hasRole('ROLE_ADMIN')) {
+            if ($note->getUser()->getId() != $user->getId()) {
+              // Only admin can see all notes.
+              throw $this->createAccessDeniedException('Unauthorized to see this note.');
+            }
+          }
+        } else {
+          throw $this->createNotFoundException('Note not found.');
+        }
+      }
+      else {
+        $note = new Note();
+        $note->setUser($this->getUser());
+        $response_code = 201;
+      }
+      $title = $request->get('title');
+      $body = $request->get('body');
+      $note->setTitle($title);
+      $note->setBody($body);
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($note);
+      $em->flush();
+
+      $view = $this->view($note, $response_code);
+      return $this->handleView($view);
+    }
 }
